caaf3f507b18472b18781acb773f0bfe901c6596
Ahmed Ali <ahmedawan43011@gmail.com>
2022-04-06 14:56:13 +0500

    rendering list through map and filter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 
 import './App.css';
-//Array 
-const people =[
-  'Ahmed Ali:Jr Software Engineer',
-  'Shahzaib Mumtaz:Jr Iso Developer',
-  'Hasnain Ali Baloch: Team Lead <3',
-  'Fahad Sohail:Front-End Developer'
-]
+import people from './Data/data';
+import { getImageUrl } from './Utils/utils';
 //Function Component
 function List() {
-  //Through array map to show your list
-  const listItems = people.map(person =>
-    <li>{person}</li>
+  //Filter chemist through array of object(people)
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
   );
-  //return your list 
-  return <ul>{listItems}</ul>
+  //then array map method to get image through utils file
+  const listItems = chemists.map(person =>
+    <li>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+       {/* getting key and value from array of object chemist data.js file */}
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return <ul>{listItems}</ul>;
 }
 
 export default List;
